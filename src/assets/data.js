fdf72813f41a6c5cfa4c32f0175f369549c46a28
Mui Kai En <muikaien1@gmail.com>
2017-04-28 23:09:49 +0800

    Replace placeholder questions

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,33 +1,33 @@
 export default {
   questions: [
     {
-      text: "Hi",
-      image: "logo.png",
+      text: "Where is this location?",
+      image: "http://i.imgur.com/gjHAjb7.png",
       answers: [
-        { text: "There", correct: true },
-        { text: "Bye", correct: false },
-        { text: "Nah", correct: false },
-        { text: "fdajios", correct: false }
+        { text: "Former Dunman High School", correct: true },
+        { text: "Chung Cheng High School (Main)", correct: false },
+        { text: "Bukit Panjang High School", correct: false }
       ]
     },
     {
-      text: "Bye",
-      image: "logo.png",
+      text: "Dunman High School's logo was designed by:",
+      image: "http://edumatters.sg/wp-content/uploads/2014/04/Dunman-High-School2-1024x1024.jpg",
       answers: [
-        { text: "Ta", correct: true },
-        { text: "What", correct: false },
-        { text: "Nfjfdsafaweio", correct: false },
-        { text: "fdajfjaeiwfs23ios", correct: false }
+        { text: "Mr. Sun Hwan Sin", correct: true },
+        { text: "Liu Kang", correct: false },
+        { text: "Mr. Tan Thiam Hock", correct: false }
       ]
     },
     {
-      text: "Good",
-      image: "logo.png",
-      answers: [
-        { text: "Evening", correct: true },
-        { text: "Boyo", correct: false },
-        { text: "bafdsd", correct: false },
-        { text: "fajitas", correct: false }
+      text: "What does this image show?",
+      image: "http://68.media.tumblr.com/tumblr_leqrx5hnZn1qa5di0.jpg",
+      answers: [      
+        { text: "Dunman High School’s school song", correct: true },
+        { text: "Dunman High School’s vision", correct: false },
+        { text: "Dunman High School’s school crest", correct: false }
+
+
+
       ]
     }
   ]
