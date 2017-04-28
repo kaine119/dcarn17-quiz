b854c7990ea84708cd52fce40f4c451d32bcadd6
Mui Kai En <muikaien1@gmail.com>
2017-04-28 23:41:52 +0800

    Add travis CI support (hopefully)

---
+++ .travis-ghpages-deploy.sh
@@ -0,0 +1,10 @@
+cd dist
+git init
+
+git config user.name "Travis CI"
+git config user.email "mui.kaien@dhs.sg"
+
+git add .
+git commit -m "[TRAVIS] Deploy to github pages"
+
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
