d719498786e946d5f8658b89d15e964ef5869326
Mui Kai En <muikaien1@gmail.com>
2017-04-29 22:26:49 +0800

    Add new questions
    New question objects in src/assets/data.js, new images in
    src/assets/question-imgs.

--- src/assets/data.js
+++ src/assets/data.js
@@ -16,8 +16,8 @@ export default {
       text: "Dunman High School's logo was designed by:",
       image: "Dunman-High-School2-1024x1024.jpg",
       answers: [
-        { text: "Mr. Sun Hwan Sin", correct: true },
-        { text: "Liu Kang", correct: false },
+        { text: "Mr. Sun Hwan Sin", correct: false },
+        { text: "Liu Kang", correct: true },
         { text: "Mr. Tan Thiam Hock", correct: false }
       ]
     },
@@ -28,9 +28,60 @@ export default {
         { text: "Dunman High School’s school song", correct: true },
         { text: "Dunman High School’s vision", correct: false },
         { text: "Dunman High School’s school crest", correct: false }
-
-
-
+      ]
+    },
+    {
+      text: "What do you think Dunman High School’s metal buttons symbolise?",
+      image: "Photo_Grid_1483335829713.png",
+      answers: [
+        { text: "Dunmanians’ courage and determination", correct: false },
+        { text: "Dunman High School’s traditional Chinese culture", correct: true },
+        { text: "Dunman High School’s status as an autonomous school", correct: false }
+      ]
+    },
+    {
+      text: "The above mascots for Dunman High School are known as...",
+      image: "fEd86dW.png",
+      answers: [
+        { text: "Dunman Dolls", correct: true },
+        { text: "Dunman Figurines", correct: false },
+        { text: "Dunman Puppets", correct: false }
+      ]
+    },
+    {
+      text: "Dunman High School celebrated its 60th anniversary in the year...",
+      image: "0808fdhs60_01.jpg",
+      answers: [
+        { text: "1990", correct: false },
+        { text: "2007", correct: false },
+        { text: "2016", correct: true }
+      ]
+    },
+    {
+      text: "The above photo is taken at Dunman High School’s...",
+      image: "1083_03.jpg",
+      answers: [
+        { text: "Media Resource Library (MRL)", correct: true },
+        { text: "Auditorium", correct: false },
+        { text: "PAC", correct: false }
+      ]
+    },
+    {
+      text: "Dunman High School’s Performing Arts Centre (PAC) can sit up to...",
+      image: "article_10180.jpg",
+      answers: [
+        { text: "100 people", correct: false },
+        { text: "400 people", correct: false },
+        { text: "800 people", correct: true }
+      ]
+    },
+    {
+      text: "This was layout of the former Dunman High School campus. \n Which of the following building names are no longer in use?",
+      image: "28swj8n.jpg",
+      answers: [
+        { text: "集思楼", correct: false },
+        { text: "晓学楼", correct: true },
+        { text: "正心楼", correct: false }
       ]
     }
   ]
